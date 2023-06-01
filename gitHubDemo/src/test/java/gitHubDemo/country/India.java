bb2b2781bece50be5cb9c76019e3331bbf19cb15
RavishankarJanghel <ravijanghel@gmail.com>
2023-06-01 09:26:19 +0530

    2nd commit.

--- gitHubDemo/src/test/java/gitHubDemo/country/India.java
+++ gitHubDemo/src/test/java/gitHubDemo/country/India.java
@@ -5,6 +5,7 @@ public class India {
 	public static void main(String[] args) {
 		System.out.println("This is India.");
 		System.out.println("India is fastest growing country.");
+		System.out.println("There are 27 States in india.");
 	}
 
 }
